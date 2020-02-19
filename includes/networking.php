9bec1f158e99a910a2678be70741591110bb407c
Jose <joseantoniomazon@gmail.com>
2020-02-19 03:46:49 +0100

    revert changes to php files, touch js instead

--- includes/networking.php
+++ includes/networking.php
@@ -15,34 +15,6 @@ function DisplayNetworkingConfig()
     foreach ($interfaces as $interface) {
         exec("ip a show $interface", $$interface);
     }
-
-    if ($fh = fopen('/etc/systemd/network/bridge-br0.network', 'r')) {
-        $dns_ocurrences = 0;
-        while (!feof($fh)) {
-            $line = fgets($fh);
-            if (strpos($line, "Address")) {
-                $ip_and_netmask = explode("=", $line)[1];
-                $ip = explode("/", $ip_and_netmask)[0];
-                $netmask = "/" . explode("/", $ip_and_netmask)[1];
-                continue;
-            }
-            if (strpos($line, "Gateway")) {
-                $gw = explode("=", $line)[1];
-                continue;
-            }
-            if (strpos($line, "DNS")) {
-                $dns_ocurrences++;
-                if ($dns_ocurrences == 1) {
-                    $dns1 = explode("=", $line)[1];
-                }
-                elseif ($dns_ocurrences == 2) {
-                    $dns2 = explode("=", $line)[1];
-                }
-                continue;
-            }
-        }
-        fclose($fh);
-    }
     
-    echo renderTemplate("networking", compact("status", "interfaces", "ip", "netmask", "gw", "dns1", "dns2"));
+    echo renderTemplate("networking", compact("status", "interfaces"));
 }
